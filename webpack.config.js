251e9f7b9d42646c8f1659c575515d5dd45838e1
Farid Babayev <farid.b@code.edu.az>
2018-03-09 03:08:46 +0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: './src/canvas.js',
+    output: {
+        path: __dirname + '/dist/js',
+        filename: 'canvas.bundle.js'
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['dist'] },
+            files: ['./dist/*']
+        }),
+    ],
+    watch: true,
+    devtool: 'source-map'
+};
